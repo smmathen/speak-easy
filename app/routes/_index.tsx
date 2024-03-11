1bc5b43ad95de64c151687f728858d608b6c794f
Shawn Mathen <sgmathen@gmail.com>
2024-03-11 13:22:58 -0500

    data pull from messages table

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,16 @@
-import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import supabase from "utils/supabase"
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+
+export const loader = async () => {
+  const { data } = await supabase.from('messages').select();
+  return data;
+}
+
 
 export default function Index() {
+  const data = useLoaderData();
   return (
-    <h1>howdy ags</h1>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
   );
 }
